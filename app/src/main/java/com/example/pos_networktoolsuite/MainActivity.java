5ec7645f4593368a18a9b3494806185b03956115
sarahp284 <prisad16@htl-kaindorf.at>
2020-03-02 09:08:01 +0100

    First Commit

--- app/src/main/java/com/example/pos_networktoolsuite/MainActivity.java
+++ app/src/main/java/com/example/pos_networktoolsuite/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.pos_networktoolsuite;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.pos_networktoolsuite.networkscan.ArpClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnClick = (Button) findViewById(R.id.button) ;
+        btnClick.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                ArpClient arp=new ArpClient();
+                arp.pingservice();
+
+            }
+        });
+
+
     }
+
 }
